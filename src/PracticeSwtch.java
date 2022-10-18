b5ef2e62b1599ed3a1ee16bf0d4f375d02ef1683
C:/Users/Haris Niazi <muhammadharisniaxi@gmail.com>
2022-10-18 12:23:02 +0500

    Changes

--- src/PracticeSwtch.java
+++ src/PracticeSwtch.java
@@ -1,12 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.Scanner;
 
 public class PracticeSwtch {
-    public static void main(String[] args) throws IOException {
-        // Scanner sc=new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Any Alphabets from A to Z : ");
+        String Alphabet = sc.nextLine();
+        sc.close();
+        // System.out.println("Your Alphabet is " + Alphabet);
 
+        switch (Alphabet) {
+            case "A":
+                System.out.println("The Word you enter is Vowel");
+                break;
+            case "E":
+                System.out.println("The Word you enter is Vowel");
+                break;
+            case "I":
+                System.out.println("The Word you enter is Vowel");
+                break;
+            case "O":
+                System.out.println("The Word you enter is Vowel");
+                break;
+            case "U":
+                System.out.println("The Word you enter is Vowel");
+                break;
+
+            default:
+                System.out.println("The Word you enter is consonant");
+                break;
+        }
+
+    }
 }
