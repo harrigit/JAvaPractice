c7ead14d39185fde57783d7afaf2c7a985d33bd0
C:/Users/Haris Niazi <muhammadharisniaxi@gmail.com>
2022-10-18 11:27:42 +0500

    Updated

---
+++ src/PracticeSwtch.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PracticeSwtch {
+    public static void main(String[] args) throws IOException {
+        // Scanner sc=new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+    }
+
+}
